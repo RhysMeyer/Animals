a18aa6ccafc1e8c7d97e684052e4212ef753d621
RhysMeyer <rhys.a.meyer@gmail.com>
2022-01-25 09:59:01 +0000

    Added breathing test

--- src/main/java/com/TSI/Rhys/Meyer/Animals/Bat.java
+++ src/main/java/com/TSI/Rhys/Meyer/Animals/Bat.java
@@ -2,13 +2,6 @@ package com.TSI.Rhys.Meyer.Animals;
 
 public class Bat extends Mammal{
 
-    @Override
-    public Animal reproduce() {
-        return null;
-    }
 
-    @Override
-    public String Breathe() {
-        return null;
-    }
+
 }
